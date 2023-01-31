150c40977f8a00368f4eccae7dacf922109f189c
superiorkid <bayuPetani@protonmail.com>
2023-02-01 00:45:27 +0800

    complete card

--- src/components/Tags/Tags.tsx
+++ src/components/Tags/Tags.tsx
@@ -1,20 +1,2 @@
-import { Tag, TagLabel, TagLeftIcon } from "@chakra-ui/react";
-import { CiHashtag } from "react-icons/ci";
-import React, { FC } from "react";
 
-type Props = {
-  tag: ITag;
-};
-
-const Tags: FC<Props> = ({ tag }) => {
-  return (
-    <>
-      <Tag size="sm" key="sm" variant="subtle" colorScheme="cyan">
-        <TagLeftIcon boxSize="12px" as={CiHashtag} />
-        <TagLabel>{tag.name}</TagLabel>
-      </Tag>
-    </>
-  );
-};
-
-export default Tags;
+export default Tags

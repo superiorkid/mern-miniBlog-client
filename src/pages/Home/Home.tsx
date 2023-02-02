2e8192892bad29e9b3c8ce7621eb60063c46f72b
superiorkid <bayuPetani@protonmail.com>
2023-02-02 22:15:53 +0800

    implement react-query to fetching data

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,14 +2,27 @@ import React, {FC} from "react";
 import Layout from "../../components/Layout/Layout";
 import {Link} from "react-router-dom";
 import PostCard from "../../components/PostCard/PostCard";
-import {Flex, Heading} from "@chakra-ui/react";
+import {Box, Button, Flex, Heading, Spacer} from "@chakra-ui/react";
 
 
 const Home: FC = () => {
+    const {data, isLoading, isError, error} = useQuery<IPost[], Error>("post", fetchPosts)
+
+    if (isLoading) {
+        return (
+            <Box>
+                loading...
+            </Box>
+        )
+    }
+
+    if (isError) {
+        return <Box>{error.message}</Box>
+    }
+
     return (
         <Layout>
             <Flex direction="column" gap={3} p={2}>
-                <PostCard/>
                 <PostCard/>
             </Flex>
         </Layout>

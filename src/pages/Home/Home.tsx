dd8c444a202d2f51e78baa1261c7b2d46115b888
superiorkid <bayuPetani@protonmail.com>
2023-02-02 15:08:49 +0800

    complete write article features

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,2 +1,19 @@
+import React, {FC} from "react";
+import Layout from "../../components/Layout/Layout";
+import {Link} from "react-router-dom";
+import PostCard from "../../components/PostCard/PostCard";
+import {Flex, Heading} from "@chakra-ui/react";
+
+
+const Home: FC = () => {
+    return (
+        <Layout>
+            <Flex direction="column" gap={3} p={2}>
+                <PostCard/>
+                <PostCard/>
+            </Flex>
+        </Layout>
+    )
+}
 
 export default Home

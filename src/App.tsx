381c2ffead014a5924790117c7c7633ee98e389a
superiorkid <bayuPetani@protonmail.com>
2023-02-03 22:43:29 +0800

    user login done using react-query

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,16 @@ import React, {FC} from "react";
 import {Route, Routes} from "react-router-dom";
 import Home from "./pages/Home/Home";
 import UserProfile from "./pages/UserProfile/UserProfile";
+import WriteNewArticle from "./pages/WriteNewArticle/WriteNewArticle";
 
 
 const App: FC = () => {
     return (
         <Routes>
             <Route path="/" element={<Home/>} />
+            <Route path="/detail/:slug" element={<DetailPost/>} />
             <Route path="/profile" element={<UserProfile/>} />
+            <Route path="/write-article" element={<WriteNewArticle/>} />
         </Routes>
     )
 }

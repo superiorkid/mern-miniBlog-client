c54580d341b3111b5a0b1b72e67c4d7856ef99e9
superiorkid <bayuPetani@protonmail.com>
2023-02-04 11:20:01 +0800

    implement public route

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,9 @@ import {Route, Routes} from "react-router-dom";
 import Home from "./pages/Home/Home";
 import UserProfile from "./pages/UserProfile/UserProfile";
 import WriteNewArticle from "./pages/WriteNewArticle/WriteNewArticle";
+import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
+import DetailPost from "./pages/DetailPost/DetailPost";
 
 
 const App: FC = () => {
@@ -13,6 +16,8 @@ const App: FC = () => {
             <Route path="/detail/:slug" element={<DetailPost/>} />
             <Route path="/profile" element={<UserProfile/>} />
             <Route path="/write-article" element={<WriteNewArticle/>} />
+            <Route path="/login" element={<Login/>} />
+            <Route path="/register" element={<Register/>} />
         </Routes>
     )
 }

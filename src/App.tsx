e9647319a15cd12e7d62b51486937b7a14b0eac5
superiorkid <bayiPetani@protonmail.com>
2023-02-11 21:59:15 +0800

    add protected route to pages

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import Login from "./pages/Login/Login";
 import Register from "./pages/Register/Register";
 import DetailPost from "./pages/DetailPost/DetailPost";
 import PublicRoute from "./utils/PublicRoute";
+import ProtectedRoute from "./utils/ProtectedRoute";
 import EditPost from "./pages/EditPost/EditPost";
 import Tag from "./pages/Tag/Tag";
 
@@ -16,10 +17,23 @@ const App: FC = () => {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/tag/:tag" element={<Tag />} />
-      <Route path="/edit/:slug" element={<EditPost />} />
+      <Route
+        path="/edit/:slug"
+        element={
+          <ProtectedRoute>
+            <EditPost />
+          </ProtectedRoute>
+        }
+      />
       <Route path="/detail/:slug" element={<DetailPost />} />
-      <Route path="/profile" element={<UserProfile />} />
-      <Route path="/write-article" element={<WriteNewArticle />} />
+      <Route
+        path="/write-article"
+        element={
+          <ProtectedRoute>
+            <WriteNewArticle />
+          </ProtectedRoute>
+        }
+      />
       <Route
         path="/login"
         element={
